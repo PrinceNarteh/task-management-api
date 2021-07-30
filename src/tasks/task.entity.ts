6e414338d7f1e0dea6806ddb9067ca9cf6723f48
Prince Narteh <prinarteh@gmail.com>
2021-07-30 14:39:18 +0000

    Add hashing of password

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 
-@Entity()
+@Entity({ name: 'tasks' })
 export class Task {
   @PrimaryGeneratedColumn('uuid')
   id: string;
